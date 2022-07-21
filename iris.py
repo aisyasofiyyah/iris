d4240567451f99bdaeaf9fae712aad5d3895ca6a
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-21 22:23:42 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -8,7 +8,6 @@ This app predicts the **Iris flower** type.
 """)
 
 st.sidebar.header('User Input Parameters')
-st.image('setosa.png', width=200)
 
 def user_input_features():
     sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
@@ -21,6 +20,8 @@ def user_input_features():
             'Petal Width': petal_width}
     features = pd.DataFrame(data, index=[0])
     return features
+
+st.sidebar.image('setosa.png', width=200)
 
 df = user_input_features()
 
