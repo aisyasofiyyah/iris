e4024ea85b9da42606eaa7fbb872e32abdfb3c9b
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-21 22:43:39 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -34,7 +34,17 @@ clf.fit(X, Y)
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
-st.image('flowers.png', width=300)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image('flowers.png', center, width=500)
+
+with col3:
+    st.write(' ')
+    
 st.write(pd.DataFrame({
         'Species': ['Iris Setosa','Iris Versicolor','Iris Virginica'],}))
 
