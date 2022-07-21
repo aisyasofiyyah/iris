bad0625786218b0775df09472ac40ecc873d789a
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-21 22:28:48 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -37,16 +37,19 @@ prediction_proba = clf.predict_proba(df)
 #st.write(pd.DataFrame({
         #'Species': ['Iris Setosa','Iris Versicolor','Iris Virginica'],}))
 
-col1, col2, col3 = st.beta_columns([1,6,1])
+col1, col2, col3 = st.columns(3)
 
 with col1:
-st.write("In")
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-st.image("setosa.png")
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
-st.write("Iris Setosa'")
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 
 tab1, tab2 = st.tabs(["User Input Parameters", "Results"])
 
