0431617dc62491383644d97fe585092f5dd31e34
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-22 22:32:48 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -37,7 +37,7 @@ clf.fit(X, Y)
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
-col1, col2, col3 = st.columns([2,1,3])   
+col1, col2, col3 = st.columns([3,1,2])   
  
 with col1:
     st.image('flowers.png', width=500)
