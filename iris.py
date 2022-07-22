52496d63391dedb106da716a749b0c5a6c845580
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-22 22:57:45 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -38,7 +38,7 @@ clf.fit(X, Y)
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
-col1, col2, col3 = st.columns(3)   
+col1, col2, col3 = st.columns([3,1,2])   
  
 with col1:
    st.write(' ')
