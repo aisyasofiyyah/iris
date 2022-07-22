aeb97066ac8c67b89748c927594ecd4c829c3b40
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-22 20:55:08 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -10,7 +10,7 @@ primary_clr = st.get_option("theme.primaryColor")
 
 st.markdown("<h1 style='text-align: center;'>✿ Iris Flower Prediction App ✿</h1>", unsafe_allow_html=True)
 
-st.sidebar.image('setosa.png', use_column_width= 'never')
+st.sidebar.image('setosa.png', width= none)
 st.sidebar.write("This app predicts the **Iris flower** type.")
 st.sidebar.header('User Input Parameters')
 
