ee067f05f34f9bcb06afe005031b6d4d1d9d998a
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-22 21:12:12 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -15,6 +15,7 @@ st.sidebar.markdown("<h2 style='text-align: center;'>User Input Parameters</h2>"
 
 def user_input_features():
     sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    st.write('Values:', sepal_length)
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
