d8db2d30ff38ca030369eb5c040b85aca387b9fb
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-22 19:19:16 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -35,9 +35,9 @@ prediction_proba = clf.predict_proba(df)
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.write("This app predicts the **Iris flower** type.")
     st.write(pd.DataFrame({
-          'Species': ['Iris Setosa','Iris Versicolor','Iris Virginica'],}))
+          'Species': ['Iris Setosa','Iris Versicolor','Iris Virginica'],}))    
+    st.write("This app predicts the **Iris flower** type.")
 
 with col2:
     st.image('flowers.png', width=500)
